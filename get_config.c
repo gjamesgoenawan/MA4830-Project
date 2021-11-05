dda9c3cf622acd95850a8858b2e22e396dff5034
Andhika Satriya Bhayangkara <85143700+winQe@users.noreply.github.com>
2021-11-06 02:33:43 +0800

    fixed error

--- get_config.c
+++ get_config.c
@@ -7,22 +7,22 @@ int main()
     FILE *config = fopen("config.txt","r+"); //open file and assign it to a handle
     char * line = NULL;
     size_t len = 0;
-    ssize_t read;
-    char *params[10];
-    int i=0;
+    char params[2][10];
 
     if(!config) { //error handling if cannot open config file
         perror("Cannot open config file!");
         exit(1);
     }
 
-    while ((read = getline(&line, &len, config)) != -1) {
-        line = strstr(line,"=");
-        params[i] = &line[1];
-        printf("%s\n",params[i]);
+    int i=0;
+    while ((getline(&line, &len, config)) != -1) { //reads line by line in the config file
+        line = strtok(line,"\n"); //remove break lines
+        line = strstr(line,"="); //only look at the value after = sign
+        strcpy(params[i],&line[1]); //store the value into an array
         i++;
     }
-    printf("%s\n",params[0]);
+    puts(params[0]);
+
     fclose(config);
 return 0;
 }
