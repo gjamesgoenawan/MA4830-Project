bb3bda6fd2cc338dd4d81d63457c65953a5d9e5b
Andhika Satriya Bhayangkara <85143700+winQe@users.noreply.github.com>
2021-11-11 01:30:06 +0800

    keyboard input with ncurses

--- scratch2.c
+++ scratch2.c
@@ -1,10 +1,40 @@
-#include <stdio.h>
+#include<ncurses.h>
 
 int main()
 {
-    int a,ch;
-    ch = getch();
-    a = getch();
-    printf("%d %d",ch,a);
+int amplitude = 50, frequency = 20;
+int ch;
+
+/* Curses Initialisations */
+initscr();
+raw();
+keypad(stdscr, TRUE);
+noecho();
+
+printw("Press E to Exit\n");
+
+while((ch = getch()) != 'E')
+{
+    switch(ch)
+    {
+    case KEY_UP:         
+        amplitude++;
+        break;
+    case KEY_DOWN:      
+        amplitude--;
+        break;
+    case KEY_LEFT:      
+        frequency--;
+        break;
+    case KEY_RIGHT:     
+        frequency++;
+        break;
+    }
+    printw("\nAmplitude %d Frequency %d ",amplitude,frequency);
+}
+
+printw("\n\Exiting Now\n");
+endwin();
+
 return 0;
 }
